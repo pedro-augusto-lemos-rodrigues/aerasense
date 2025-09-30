b4c6bfb76dfd295a2ada0ab8a529634f023ba2ed
Murilo2305 <murilo.vieira@sptech.school>
2025-09-30 19:18:12 -0300

    Banco de Dados V1
    A primeira versão do banco de dados da Aerosense.

---
+++ BD-sql/BD_V1.sql
@@ -0,0 +1,87 @@
+create database aerosense;
+
+use aerosense;
+
+/*
+Na tabela empresa será guardada a lista completa das empresas que atendemos, suas informações, contato, plano e situação do seu pagamento
+*/
+create table empresa(
+idEmpresa int primary key auto_increment,
+cnpj char(14) not null unique,
+nome varchar(45) not null,
+email varchar(45) not null,
+telefone varchar(45),
+preco_contrato int not null,
+plano varchar(20),
+constraint chkPlano
+	check(plano in ('mensal','Trimestral','Semestral','Anual')),
+pagEmDia boolean
+);
+
+insert into empresa values
+	(default,12345678910111,'Empresa 1','Empresa1@email','(11) 912345678',18000,'Trimestral',true),
+	(default,12345678910112,'Empresa 2','Empresa2@email','(11) 987654321',50000,'Semestral',true);
+  
+/*
+Na tabela funcinario serão inseridas as informações dos funcionários individuais que possuem acesso as nossas dashboards e a qual empresa cada
+um está associado
+*/
+  
+create table funcionario(
+idFuncionario int primary key auto_increment,
+cpf char(11) not null unique,
+nome varchar(45) not null,
+email varchar(45) not null,
+telefone varchar(45),
+senha varchar(45) unique,
+fkEmpresa int not null,
+constraint fkEmpresaFuncionario
+	foreign key(fkEmpresa)
+    references empresa(idEmpresa)
+);
+
+insert into funcionario values
+	(default,'12345678910','Pedro','Pedro@email','(11) 912345678','SENHAPEDRO',1),
+	(default,'12345678911','Marcos','Marcos@email','(11) 987654321','SENHAMARCOS',2);
+    
+/*
+Individual é um termo placeholder. Cada sensor teria uma tabela própria que seria nomeada como o código individual do sensor, que seria um 
+código alfanumérico. Essa é a tabela que receberá os dados do sensor.
+*/
+    
+create table individual(
+idIndiviual int primary key auto_increment,
+porcentagemGas decimal(5,2),
+Dt datetime default current_timestamp
+);
+
+insert into individual values 
+	(default,'0.5',default),
+	(default,'0.3',default);
+    
+    select * from individual;
+  
+/*
+A tabela sensor listará todos os sensores do nosso sistema, seus códigos, e a qual empresa eles pertencem.
+*/
+  
+create table sensor(
+idSensor int primary key auto_increment,
+codigo char(12) unique,
+fkEmpresa int,
+constraint fkEmpresaSensor
+	foreign key(fkEmpresa)
+    references empresa(idEmpresa)
+);
+
+insert into sensor values
+	(default,'12wd34fg56tr',1),
+	(default,'09as87gb65zx',2);
+    
+select f.nome as Funcionário,
+	e.nome as empresa,
+	s.codigo as sensor
+    from sensor as s join empresa as e
+		on s.fkEmpresa = e.idEmpresa
+	join funcionario as f
+		on f.fkEmpresa = e.idEmpresa;
