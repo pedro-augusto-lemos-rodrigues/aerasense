85f377f99bf3d7957fa904efd5cfec913daebbe1
pedro-rodrigues2025 <pedroalr2005@gmail.com>
2025-10-09 22:51:14 -0300

    tentativa de um js pro login

--- pedro/login.js
+++ pedro/login.js
@@ -1,3 +1,33 @@
+var tentativas = 3
+
 function conta(){
-    
+    var email = ipt_email.value
+
+    var senha = ipt_senha.value
+
+    var emailSalvo = "clienteAerasense"
+    var senhaSalva = "12345678"
+
+    var mensagem = ""
+
+
+        while(tentativas){
+            if(email == emailSalvo && senha == senhaSalva){
+                mensagem = `Seja bem vindo`
+            } else {
+                mensagem = `Falha na autenticação`
+            }
+            
+            if (tentativas == 0) {
+                ipt_email.style.display = "none"
+                ipt_senha.style.display = "none"
+            }
+
+            tentativas --
+
+        }
+
+    div_mensagem.innerHTML = mensagem
+
+
 }
