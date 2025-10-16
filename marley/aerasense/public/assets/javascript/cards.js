a396b9b758d0efd52f12e5e2fd2810f42c1512e9
MarleyS439 <marley.santos@sptech.school>
2025-10-16 17:19:54 -0300

    Adição de comentários

--- marley/aerasense/public/assets/javascript/cards.js
+++ marley/aerasense/public/assets/javascript/cards.js
@@ -1,16 +1,32 @@
+/**
+ * Arquivo de interatividade dos cards de benefícios
+ *
+ * O método setTimeout(), recebe dois parâmetros aqui, onde ele espera uma
+ * ação e o tempo necessário para aguardar para executar tal ação.
+ *
+ * Não foi usado um forEach por não ser permitido ainda...
+ *
+ * @author Marley de S. Santos (MarleyS439) <marleysantos439@gmail.com>
+ */
+
 document.addEventListener("DOMContentLoaded", () => {
+  // Obtém o título e o card
   const card1 = document.getElementById("benefit1");
   const title1 = document.getElementById("title1");
 
+  // Obtém o título e o card
   const card2 = document.getElementById("benefit2");
   const title2 = document.getElementById("title2");
 
+  // Obtém o título e o card
   const card3 = document.getElementById("benefit3");
   const title3 = document.getElementById("title3");
 
+  // Obtém o título e o card
   const card4 = document.getElementById("benefit4");
   const title4 = document.getElementById("title4");
 
+  // Define uma velocidade de aparição do título
   let velocity = 50;
 
   card1.addEventListener("click", () => {
