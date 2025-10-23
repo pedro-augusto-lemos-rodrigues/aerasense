61dbb8032518a6068c5085cfeb2d23550f56b0e7
GabrielMarRib <gabriel.mribeiro@sptech.school>
2025-10-22 21:39:57 -0300

    Tirei os regex

--- gabriel/TelaCadastro/cadastro.js
+++ gabriel/TelaCadastro/cadastro.js
@@ -18,11 +18,54 @@ function CadastrarForms(e){
         ID_Erros.innerHTML = 'Algum campo não está devidamente preenchido!'
         return
     }
+
+
     // Valida se contem numero nos campos: Nome e sobrenome
     if( /[0-9]/.test(nome)|| /[0-9]/.test(sobrenome)){
         ID_Erros.innerHTML = 'Campo nome ou sobrenome invalido!'
         return
     }
+
+    var listaNumeroString = '1234567890'
+    var i = 0
+    var b = 0
+    var CaracNomeSobre = false
+    // Para cada letra, ele entra no segundo while
+    while(i<nome.length){ 
+        // Já no segundo while, ele ve se a letra é um dos numeros da variavel(string) listaNumeroString
+        while(b<listaNumeroString.length){
+            if(nome[i] == listaNumeroString[b]){
+                CaracNomeSobre = true
+            }
+            b++
+        }
+        b = 0
+        i++
+    }
+    i = 0
+    b = 0
+    // Repete o processo com o sobrenome
+     while(i<sobrenome.length){ 
+        while(b<listaNumeroString.length){
+            if(sobrenome[i] == listaNumeroString[b]){
+                CaracNomeSobre = true
+            }
+            b++
+        }
+        b = 0
+        i++
+    }
+    if(CaracNomeSobre){
+        ID_Erros.innerHTML = 'Campo nome ou sobrenome invalido! Contem numero'
+         return
+    }
+
+
+
+
+
+
+
      // Valida a quantidade de numeros no CNPJ obs: com os pontos e traços inclusos
     if(cnpj.length < 14){
         ID_Erros.innerHTML = 'Campo CNPJ invalido ou incompleto!'
@@ -55,44 +98,16 @@ function CadastrarForms(e){
         MinCatere = true
     }
     
-    // Deve ter mínimo 2 letras maiúsculas: ABCDEFG.
-    var i = 0
-    var count = 0
-    // Para cada letra, ele faz um if para ver se ela é maiuscula
-    while(i<senha.length){
-       if(!isNaN(senha[i])){
-            i++
-            continue
-        }else{
-            if( senha[i] == senha[i].toUpperCase()){
-                count++
-            }
-        }
-        if(count == 2){
-            CaracMax = true
-            break
-        }
-        i++
+    // Contem letra minúscula: abcdefgh.
+    if(senha.toLowerCase() != senha){
+        CaracMax = true
+    }
+    // Contem letra maiúsculas: ABCDEFG.
+    if(senha.toUpperCase() != senha){
+        CaracMin = true
     }
     
-    
-    // Deve ter mínimo 1 letra minúscula: abcdefgh.
-    i = 0
-    count = 0
-    // Para cada letra, ele faz um if para ver se ela é minuscula 
-    while(i<senha.length){
-        if(!isNaN(senha[i])){
-              i++
-            continue
-        }else{
-            if( senha[i] == senha[i].toLowerCase()){
-                count++
-                CaracMin = true
-                break
-            }
-        }
-        i++
-    }
+   
 
     i = 0
     // Tem no mínimo 1 número: 123456.
@@ -146,6 +161,7 @@ function CadastrarForms(e){
     if(!CaracSimb){
         ID_Erros.innerHTML += `Sua senha deve ter no mínimo 1 símbolos: !@#$%^&*<br>`
     }
+
     if(MinCatere&&CaracMax&&CaracMin&&CaracNumero&&CaracSimb){
         alert("Cadastro realizado com sucesso!")
         window.location.href = './../../marley/aerasense/index.html';
@@ -153,67 +169,58 @@ function CadastrarForms(e){
     
 }
 function mascaracelular(){
-        let input = event.target;
-        let valor = input.value;
-
-        // Remove caracteres não numéricos
-        // g → significa “global”, ou seja, ele vai procurar em toda a string, não só na primeira ocorrência.
-        // \D → qualquer caractere que não seja um dígito (ou seja, tudo que não é 0–9).
-
-        // Dá um replace nos carateres que são LETRAS(De 'A' a 'Z', tanto minusculo quanto maiuculo)
-        valor = valor.replace(/\D/g, '');
-        // Limita a variavel valor para somente 11 caracteres
-        valor = valor.substring(0,11)
-
-        // Aplica a máscara (XX) XXXXX-XXXX
-
-        //Quando a varivel passa de 3 caracteres(cotando com o zero)
-        if (valor.length > 2) {
-                // Aplica aos 2 primeiros caractestes o: '()' Ex: (11) XXX
-            valor = `(${valor.substring(0, 2)}) ${valor.substring(2)}`;
-        }
-
-        if (valor.length > 10) {
-            // Aplica apos os 10 primeiros caractestes um: '-' Ex: (11) XXXX-XXX
-            valor = `${valor.substring(0, 10)}-${valor.substring(10)}`;
-        }
         
+        var valor = input_celular.value;
+        var i = 0
+        // input_celular = valor.substring(0,8)
+        
+        while(i<valor.length){
+                if(isNaN(valor[i]) || valor[i] == ' '){
+                    valor = valor.replace(valor[i], '') 
+                    input_celular.value = valor
+                    
+                    
+                }
+                i++;
+        }
 
-        input.value = valor;
+        if(valor.length > 2){
+              valor = `(${valor.substring(0, 2)}) ${valor.substring(3, valor.length)}`
+             input_celular.value = valor
+          }
+    
+        // Limita a variavel valor para somente 11 caracteres
+        input_celular.value = valor.substring(0,13)
+
 
 }
 function mascaracpnj(){
-    valor = input_CNPJ.value.replace(/\D/g, '');
-    valor = valor.substring(0,15)
-   
+    valorcnpj = input_CNPJ.value.substring(0,15)
     
-  // Regex complexo, vamos lá
-  // Todo Regex começa e termina assim:  /  /  
-  // ^(\d{2})  pega os DOIS({2}) primeiros(^) numeros(\d) dá variavel valor
-  // (\d) pega todos os NUMEROS
+        var i =0 
+        while(i<valorcnpj.length){
+                if(isNaN(valorcnpj[i]) || valorcnpj[i] == ' '){
+                    valorcnpj = valorcnpj.replace(valorcnpj[i], '') 
+                    input_celular.value = valorcnpj
 
+                }
+                i++;
+        }
 
-  // replace(ValorX, ValorY) muda o valorX pelo valorY
-  // replace(/^(\d{2})(\d)/, '$1.$2')
-  // ' /^(\d{2}) (\d)/ ' tem dois valores ai: 
-
-  // 1: " /^(\d{2}) "  
-  // E  
-  // 2: " (\d)/ "
-
-  //  '$1.$2'   $1 é o primeiro valor(/^(\d{2})) e o $2 é o segundo((\d)/)
-  // Agora é adicionar um ponto no meio desses valores '$1 . $2' 
-
-  // Essa logica se aplica para todos os regex seguintes
-
-  // mascara do CNPJ
-  // 
-  valor = valor.replace(/^(\d{2})(\d)/, '$1.$2');
-  valor = valor.replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3');
-  valor = valor.replace(/\.(\d{3})(\d)/, '.$1/$2');
-  valor = valor.replace(/(\d{4})(\d)/, '$1-$2');
-    input_CNPJ.value = valor
+  //  "00.000.000/0001-XX"
+/* 
+if(valorcnpj.length > 2){
+    valorcnpj = `${valorcnpj.substring(0, 2)}.${valorcnpj.substring(3, valorcnpj.length)}`
 }
+else if(valorcnpj.length > 6){
+    valorcnpj += `${valorcnpj.substring(0, 6)}.${valorcnpj.substring(7, valorcnpj.length)}`
+}
+
+*/
+
+    input_CNPJ.value = valorcnpj
+}
+
 function FnOcultarSenha(senha){
     // 1 - Input Senha
     // 2 - Input confirmar senha
