9053820c6d162275f3a98f4d9ce460a96387d976
pedro-rodrigues2025 <pedroalr2005@gmail.com>
2025-10-29 00:20:10 -0300

    protótipo do BD, calculadora e login feitos

--- pedro/login.js
+++ pedro/login.js
@@ -1,48 +1,39 @@
-var tentativas = 3
+
 
 function conta(){
     var email = ipt_email.value
 
     var senha = ipt_senha.value
 
-    var emailSalvo = "clienteAerasense"
-    var senhaSalva = "12345678"
+    var emailSalvo = "f.brandao@empresa.com"
+    var senhaSalva = "Sptech#2025"
 
     var mensagem = ""
-
-
-        while(tentativas > 0){
-            
-            if(email == emailSalvo && senha == senhaSalva){
-
-            mensagem = "Seja bem-vindo"
-            tentativas = 0
+    
+    
+    if(email == emailSalvo && senha == senhaSalva){
         
-        } else {
-            
-            tentativas--                    
-            
-            if (tentativas > 0) {
-                mensagem = `E-mail ou senha incorretos!<br>Restam ${tentativas} tentativa(s)`;
-            
-            } else {
-                mensagem = "Número máximo de tentativas atingido. Login bloqueado! Peça auxílio a um responsável";
-                
-                ipt_email.style.display = "none";
-                ipt_senha.style.display = "none";
+        mensagem = "Seja bem-vindo"
+        
+        ipt_email.style.backgroundColor = ""
+    
+    } else {
+        
+        mensagem = "Email ou senha incorretos"
+        
+        ipt_email.style.border = "2px solid red"
+
+        
  
  
  
  
  
             }
-            break 
-            // usada para interromper imediatamente um loop
-            // sai imediatamente do loop
-            // É útil para parar o loop antes da condição final do loop ou evitar rodar código indesejado.
+
+
+
+
+            div_mensagem.innerHTML = mensagem
         }
-        div_mensagem.innerHTML = mensagem
-    }
-
-
-}
+       
